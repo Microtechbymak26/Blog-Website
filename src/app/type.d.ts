bacf53ddc8e3e0b57035c4b6123c11f1f3ec2b2d
Microtechbymak26 <usubka786@Gmail.com>
2025-01-06 10:12:43 -0800

    more changesssssss

--- src/app/type.d.ts
+++ src/app/type.d.ts
@@ -1,7 +1,21 @@
-type Blog = {
-    Title: string,
-    Paragraph: string,
-    image: any,
-    slug: string,
-    block:string 
-}
+// type Blog =  {
+//     Title: string,
+//     Paragraph: string,
+//     image: any,
+//     slug: string,
+//     block:string 
+// }
+
+
+interface Image {
+    src: string; 
+    alt: string; 
+  }
+  
+  type Blog = {
+    Title: string;
+    Paragraph: string;
+    image: Image; 
+    slug: string;
+    block: string;
+  };
